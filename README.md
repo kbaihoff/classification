91bda31a724ce0ddf0a3e62939a94e8d3cd62b35
Kai Hoffman <hoffm154@purdue.edu>
2017-06-15 23:28:26 -0400

    Add Davey's version

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
-# Classification
-make lists go into categories
+# Jeffervescence
+
+## Day 3 Homework
+
+* Store the flicks/dinosaurs in an array, as well as in the DOM.
+
+### Mega Bonus Credit
+
+* Add a promote/fav button, just like you did yesterday!
+* Add a delete button, just like yesterday!
+
+### Super Mega Bonus Credit
+
+* Add buttons to move a flick/dinosaur up and down the list.
+
+### Super Mega Bonus Credit Hyper Fighting
+
+* Persist the flicks/dinosaurs data using `window.localStorage`.  The flicks/dinosaurs should stay in the list even when the page is refreshed.
+
+## Day 4 Homework
+
+* Fix the issue with flick id values, which sometimes results in removing the wrong flick from the array (and thus from localStorage).
+
+### Bonus Credit
+
+* Also track the year the flick was released.
+
+### Super Mega Bonus Credit
+
+* Edit the names of flicks that are already in the list (and make sure the changes persist across page loads). Gee, it would nice if we could make that span's _content editable_ somehow.
+
+### Super Mega Bonus Credit Hyper Fighting
+
+* Have a good weekend.
 
 ## Day 5 Homework
 
--- class.js
+++ class.js
@@ -1,264 +1,257 @@
-class Class {
+class App {
   constructor(selectors) {
-    this.items = []
+    this.flicks = {}
     this.max = 0
-    this.list = document.querySelector(selectors.listSelector)
-    this.template = document.querySelector(selectors.templateSelector)
-    document.querySelector(selectors.formSelector).addEventListener('submit', this.addItemViaForm.bind(this))
-    const checkboxes = document.querySelectorAll('.checkers')
-    for (let i = 0; i < checkboxes.length; i++) {
-      checkboxes[i].addEventListener('click', this.filter.bind(this))
-    }
-    document.querySelector('#search').addEventListener('keyup', this.searchWord.bind(this))
-    document.querySelector('.title').addEventListener('click', this.titleSwap.bind(this))
-    document.querySelector('.title2').addEventListener('click', this.titleSwap.bind(this))
+
+    this.template = document
+      .querySelector(selectors.templateSelector)
+
+    this.listen(selectors)
+    this.setupLists(selectors.listSelector)
     this.load()
   }
 
-  load() {
-    const itemsJSON = localStorage.getItem('itemArray')
-    if (itemsJSON != null && itemsJSON != 'undefined') {
-      const itemsArray = JSON.parse(itemsJSON)
-      if (itemsArray) {
-        itemsArray.reverse().map(this.addItem.bind(this))
+  setupLists(listSelector) {
+    this.lists = {}
+    const genres = ['comedy', 'drama', 'sci-fi']
+    genres.map(genre => {
+      this.lists[genre] = document.querySelector(`#${genre} ${listSelector}`)
+    })
+  }
+
+  listen(selectors) {
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', this.addFlickViaForm.bind(this))
+    document
+      .querySelector(selectors.searchSelector)
+      .addEventListener('keyup', this.search.bind(this))
+  }
+
+  search(ev) {
+    const q = ev.currentTarget.value
+    const prevMatches = Array.from(document.querySelectorAll('.flick-name strong'))
+    this.removeElements(prevMatches)
+
+    Array.from(document.querySelectorAll('.flick')).map(listItem => {
+      const nameField = listItem.querySelector('.flick-name')
+      const pattern = new RegExp(q, 'gi')
+      if (nameField.textContent.match(pattern)) {
+        listItem.classList.remove('hide')
+        nameField.innerHTML = nameField.innerHTML.replace(pattern, '<strong>$&</strong>')
+      } else {
+        listItem.classList.add('hide')
       }
+    })
+  }
+
+  removeElements(elementArr) {
+    elementArr.map(el => {
+      const parent = el.parentNode
+      while(el.firstChild) {
+        parent.insertBefore(el.firstChild, el)
+      }
+      el.remove()
+    })
+  }
+
+  load() {
+    // Get the JSON string out of localStorage
+    const flicksJSON = localStorage.getItem('flicks')
+
+    // Turn that into an array
+    const flicksArray = JSON.parse(flicksJSON)
+
+    // Set this.flicks to that array
+    if (flicksArray) {
+      flicksArray
+        .reverse()
+        .map(this.addFlick.bind(this))
     }
+  }
+
+  addFlick(flick) {
+    const listItem = this.renderListItem(flick)
+    this.lists[flick.genre]
+      .insertBefore(listItem, this.lists[flick.genre].firstChild)
+    
+    if (flick.id > this.max) {
+      this.max = flick.id
+    }
+
+    if (!this.flicks[flick.genre]) {
+      this.flicks[flick.genre] = []
+    }
+
+    this.flicks[flick.genre].unshift(flick)
+    this.save()
+  }
+
+  addFlickViaForm(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
+      fav: false,
+      genre: ev.target.genre.value,
+    }
+
+    this.addFlick(flick)
+
+    f.reset()
+    ev.target.flickName.focus()
   }
 
   save() {
-    localStorage.setItem('itemArray', JSON.stringify(this.items))
+    localStorage
+      .setItem('flicks', JSON.stringify(Object.keys(this.flicks).reduce((combined, genre) => {
+        return combined.concat(this.flicks[genre])
+      }, [])))
   }
 
-  renderListItem(item) {
-    const listItem = this.template.cloneNode(true)
-    listItem.classList.remove('template')
-    listItem.dataset.id = item.id
-    listItem.querySelector('.itemName').textContent = item.name
-    listItem.querySelector('.itemName').setAttribute('title', item.name)
-    
-    if (item.star) {
-      listItem.classList.add('star')
+  renderListItem(flick) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.flick-name')
+      .textContent = flick.name
+    item
+      .querySelector('.flick-name')
+      .setAttribute('title', flick.name)
+
+    if (flick.fav) {
+      item.classList.add('fav')
     }
 
-    listItem.querySelector('button.edit').addEventListener('click', this.edit.bind(this, item)) 
-    listItem.querySelector('.itemName').addEventListener('keypress', this.saveOnEnter.bind(this, item))
-    listItem.querySelector('button.star').addEventListener('click', this.starItem.bind(this, item))
-    listItem.querySelector('button.move-up').addEventListener('click', this.moveUp.bind(this, item))
-    listItem.querySelector('button.move-down').addEventListener('click', this.moveDown.bind(this, item))
-    listItem.querySelector('button.remove').addEventListener('click', this.removeItem.bind(this))
+    item
+      .querySelector('.flick-name')
+      .addEventListener('keypress', this.saveOnEnter.bind(this, flick))
 
-    return listItem
+    item
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeFlick.bind(this, flick))
+    item
+      .querySelector('button.fav')
+      .addEventListener('click', this.favFlick.bind(this, flick))
+    item
+      .querySelector('button.move-up')
+      .addEventListener('click', this.moveUp.bind(this, flick))
+    item
+      .querySelector('button.move-down')
+      .addEventListener('click', this.moveDown.bind(this, flick))
+    item
+      .querySelector('button.edit')
+      .addEventListener('click', this.edit.bind(this, flick))
+
+    return item
   }
 
-  addItem(item) {
-    const listItem = this.renderListItem(item)
-    const listHeader = document.getElementById(item.category + 'H')
-    if (listHeader == null) {
-      return false
-    }
-    else {
-      if (listHeader.style.display === 'none') {
-        listHeader.style.display = 'block'
-      }
-      const cat = '#' + item.category
-      const categorizedList = document.querySelector(cat)
-      categorizedList.insertBefore(listItem, categorizedList.firstChild)
-      if (item.id > this.max) { // If the new item has an ID greater than the "global" ID
-        this.max = item.id + 1
-      }
-      this.items.unshift(item) // Add new item to array beginning
-      this.save() // localStorage
-    }
-  }
+  removeFlick(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    const flickArr = this.flicks[flick.genre]
 
-  addItemViaForm(ev) {
-    ev.preventDefault()
-    const form = ev.target
-    const item = {
-      id: this.max + 1,
-      name: form.itemName.value,
-      star: false,
-      category: document.querySelector('#ddb').textContent,
-    }
-    if (this.addItem(item) !== false) {
-      form.reset()
-      form.childNodes[1].childNodes[3].childNodes[1].textContent = 'Choose a category'
-    }
-    else {
-      console.log('nope')
-    }
-  }
-
-  filter(ev) {
-    ev.target.classList.toggle('isChecked')
-    console.log(ev.target.classList)
-    const checkboxes = document.querySelectorAll('.checkers')
-    const categorizedLists = document.querySelectorAll('.categorList')
-    let uncheckedCats = 0
-
-    for (let i = 0; i < checkboxes.length; i++) {
-      if (!checkboxes[i].classList.contains('isChecked')) {
-        uncheckedCats++
-      }
-    }
-    if (uncheckedCats === checkboxes.length) {
-      // None of the boxes have been selected ==> no filter applied
-      for (let i = 0; i < categorizedLists.length; i++) {
-        const categorizedListItems = categorizedLists[i].childNodes
-        console.log(categorizedListItems.length)
-        if (categorizedListItems.length > 0) {
-          categorizedLists[i].previousElementSibling.style.display = 'block' // previousElementSibline === h2 for respective category
-          for (let j = 0; j < categorizedListItems.length; j++) {
-            categorizedListItems[j].style.display = 'flex'
-          }
-        }
-        else {
-          categorizedLists[i].previousElementSibling.style.display = 'none' // previousElementSibline === h2 for respective category
-        }
-      }
-    }
-    else {
-      for (let i = 0; i < checkboxes.length; i++) {
-        const thisCategory = checkboxes[i].closest('li').textContent
-        if (!checkboxes[i].classList.contains('isChecked')) {
-          // You have found an unchecked category; thisCategory is unchecked
-          for (let j = 0; j < categorizedLists.length; j++) {
-            if (categorizedLists[j].id === thisCategory) {
-              // You have obtained the ul of the unchecked category
-              categorizedLists[j].previousElementSibling.style.display = 'none' // previousElementSibline === h2 for respective category
-              const categorizedListItems = categorizedLists[j].childNodes
-              for (let k = 0; k < categorizedListItems.length; k++) {
-                // Hide all items under the list
-                categorizedListItems[k].style.display = 'none'
-              }
-            }
-          }
-        }
-        else {
-          // You have found a checked category; thisCategory is checked
-          for (let j = 0; j < categorizedLists.length; j++) {
-            if (categorizedLists[j].id === thisCategory) {
-              // You have obtained the ul of the checked category
-              categorizedLists[j].previousElementSibling.style.display = 'block' // previousElementSibline === h2 for respective category
-              const categorizedListItems = categorizedLists[j].childNodes
-              for (let k = 0; k < categorizedListItems.length; k++) {
-                // Show all items under the list
-                categorizedListItems[k].style.display = 'flex'
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  searchWord(ev) {
-    const currentSearch = ev.target.value.toLowerCase() // text in search bar
-    const listItems = document.getElementsByClassName('item') // list item elements
-    for (let i = 0; i < this.items.length; i++) { // listItems includes template ==> one too many
-      if (!listItems[i].textContent.toLowerCase().includes(currentSearch)) {
-        listItems[i].style.display = 'none'
-      }
-      else {
-        listItems[i].style.display = 'flex'
-      }
-    }
-  }
-
-  titleSwap(ev) {
-    let otherPic = ev.target.parentElement.nextElementSibling
-    if (otherPic === null) {
-      otherPic = ev.target.parentElement.previousElementSibling
-    }
-    ev.target.style.display = 'none'
-    console.log(otherPic.childNodes[1].style.display)
-    otherPic.childNodes[1].style.display = 'flex'
-  }
-
-  edit(item, ev) {
-    const listItem = ev.target.closest('.item')
-    const nameField = listItem.querySelector('.itemName')
-    const button = listItem.querySelector('.edit.button')
-    const icon = button.querySelector('i.fa')
-    if (nameField.isContentEditable) { // It currently editable
-      nameField.contentEditable = false
-      icon.classList.remove('fa-floppy-o')
-      icon.classList.add('fa-pencil')
-      button.classList.remove('success')
-      item.name = nameField.textContent
-      this.save()
-    }
-    else { // If not currently editable
-      nameField.contentEditable = true
-      nameField.focus()
-      icon.classList.remove('fa-pencil')
-      icon.classList.add('fa-floppy-o')
-      button.classList.add('success')
-    }
-  }
-
-  saveOnEnter(item, ev) {
-    if (ev.key === "Enter") {
-      ev.preventDefault()
-      this.edit(item, ev)
-    }
-  }
-
-  starItem(item, ev) {
-    const listItem = ev.target.closest('.item')
-    listItem.classList.toggle('star')
-    item.star = !item.star
-    this.save()
-  }
-
-  moveUp(item, ev) {
-    const listItem = ev.target.closest('.item')
-    const index = this.items.findIndex((current, i) => {
-      return (current.id === item.id)
-    })
-    if (index > 0) {
-      const cat = '#' + item.category
-      const categorizedList = document.querySelector(cat)
-      categorizedList.insertBefore(listItem, listItem.previousElementSibling)
-      const prev = this.items[index - 1]
-      this.items[index - 1] = item
-      this.items[index] = prev
-      this.save()
-    }
-  }
-
-  moveDown(item, ev) {
-    const listItem = ev.target.closest('.item')
-    const index = this.items.findIndex((current, i) => {
-      return (current.id === item.id)
-    })
-    if (index < this.items.length - 1) {
-      const cat = '#' + item.category
-      const categorizedList = document.querySelector(cat)
-      categorizedList.insertBefore(listItem.nextElementSibling, listItem)
-      const next = this.items[index + 1]
-      this.items[index + 1] = item
-      this.items[index] = next
-      this.save()
-    }
-  }
-
-  removeItem(ev) {
-    const listItem = ev.target.closest('.item')
-    for (let i = 0; i < this.items.length; i++) {
-      const currentId = this.items[i].id.toString()
+    // Find the flick in the array, and remove it
+    for (let i = 0; i < flickArr.length; i++) {
+      const currentId = flickArr[i].id.toString()
       if (listItem.dataset.id === currentId) {
-        this.items.splice(i, 1)
+        flickArr.splice(i, 1)
         break
       }
     }
+
     listItem.remove()
     this.save()
   }
+
+  favFlick(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    flick.fav = !flick.fav
+
+    if (flick.fav) {
+      listItem.classList.add('fav')
+    } else {
+      listItem.classList.remove('fav')
+    }
+    
+    this.save()
+  }
+
+  moveUp(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    const flickArr = this.flicks[flick.genre]
+
+    const index = flickArr.findIndex((currentFlick, i) => {
+      return currentFlick.id === flick.id
+    })
+
+    if (index > 0) {
+      this.lists[flick.genre].insertBefore(listItem, listItem.previousElementSibling)
+
+      const previousFlick = flickArr[index - 1]
+      flickArr[index - 1] = flick
+      flickArr[index] = previousFlick
+      this.save()
+    }
+  }
+
+  moveDown(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    const flickArr = this.flicks[flick.genre]
+
+    const index = flickArr.findIndex((currentFlick, i) => {
+      return currentFlick.id === flick.id
+    })
+
+    if (index < flickArr.length - 1) {
+      this.lists[flick.genre].insertBefore(listItem.nextElementSibling, listItem)
+      
+      const nextFlick = flickArr[index + 1]
+      flickArr[index + 1] = flick
+      flickArr[index] =  nextFlick
+      this.save()
+    }
+  }
+
+  edit(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    const nameField = listItem.querySelector('.flick-name')
+    const btn = listItem.querySelector('.edit.button')
+
+    const icon = btn.querySelector('i.fa')
+
+    if (nameField.isContentEditable) {
+      // make it no longer editable
+      nameField.contentEditable = false
+      icon.classList.remove('fa-check')
+      icon.classList.add('fa-pencil')
+      btn.classList.remove('success')
+
+      // save changes
+      flick.name = nameField.textContent
+      this.save()
+    } else {
+      nameField.contentEditable = true
+      nameField.focus()
+      icon.classList.remove('fa-pencil')
+      icon.classList.add('fa-check')
+      btn.classList.add('success')
+    }
+  }
+
+  saveOnEnter(flick, ev) {
+    if (ev.key === 'Enter') {
+      this.edit(flick, ev)
+    }
+  }
 }
 
-const app = new Class({
-  formSelector: '#input-form',
-  listSelector: '#item-list',
-  templateSelector: '.item.template',
+const app = new App({
+  formSelector: '#flick-form',
+  listSelector: '.flick-list',
+  templateSelector: '.flick.template',
+  searchSelector: '.search input',
 })
+
+$(document).foundation()
